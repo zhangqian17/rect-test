a4111b6438a4a5c21f61c904ee08a37d3c87ba0f
qianqian.zhang <qianqian.zhang@bkjk.com>
2018-08-29 11:01:27 +0800

    添加项目

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './pages/Home';
+// import About from './pages/About.js';
+// import Contact from './pages/Contact.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-toggleable-md">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="container">
+          <a className="navbar-brand">React Website</a>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
